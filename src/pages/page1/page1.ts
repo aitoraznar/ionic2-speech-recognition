814fd25a28296a38ba64a747b8830b0b2bea2173
Aitor Aznar Álvarez <aitor@ideable.net>
2017-02-14 13:09:16 +0100

    Impletement logic for both web and cordova

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,6 +1,6 @@
 import {Component, ViewChild, ElementRef} from '@angular/core';
-
-import {NavController} from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
+//import {SpeechRecognition} from "ionic-native";
 
 @Component({
   selector: 'page-page1',
@@ -21,51 +21,66 @@ export class Page1 {
   private recognizing: Boolean;
   @ViewChild('recognitionResult') recognitionResult: ElementRef;
 
-  constructor(public navCtrl: NavController) {
+  private initializeRecognition = () => {
+    if (!this.recognition) {
+      return;
+    }
+
+    this.recognition.lang = "es-ES";
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+
+    this.recognition.onstart = () => {
+      console.log('[SpeechRecognition]', 'start');
+      this.recognizing = true;
+      this.recognitionResult.nativeElement.innerText = '';
+    };
+
+    this.recognition.onresult = (event) => {
+      let results = Array.prototype.slice.call(event.results);
+      let result = results.find((e) => {
+        return !!e.isFinal || !!e[0].final;
+      });
+
+      if (result) {
+        let resultText = result[0].transcript;
+        console.log('[SpeechRecognition]', 'result', resultText);
+        this.recognitionResult.nativeElement.innerText = resultText;
+      }
+    };
+
+    this.recognition.onerror = (event) => {
+      console.error(event);
+    };
+
+    this.recognition.onend = () => {
+      console.log('[SpeechRecognition]', 'end');
+      this.recognizing = false;
+      this.mic = this.mic_off;
+    };
+  };
+
+  constructor(public navCtrl: NavController, public platform: Platform) {
     this.recognition = null;
     this.recognizing = false;
 
-    let _window = (<any>window);
+    this.platform.ready().then((readySource) => {
+      console.log('Platform ready from', readySource);
+      let _window = (<any>window);
 
-    if (!('webkitSpeechRecognition' in window)) {
-      alert('¡API no soportada!');
-    } else {
-
-      this.recognition = new _window.webkitSpeechRecognition();
-      this.recognition.lang = "es-ES";
-      this.recognition.continuous = true;
-      this.recognition.interimResults = true;
-
-      this.recognition.onstart = () => {
-        console.log('[SpeechRecognition]', 'start');
-        this.recognizing = true;
-        this.recognitionResult.nativeElement.innerText = '';
-      };
-
-      this.recognition.onresult = (event) => {
-        let results = Array.prototype.slice.call(event.results);
-        let result = results.find((e) => {
-          return !!e.isFinal;
-        });
-
-        if (result) {
-          let resultText = result[0].transcript;
-          console.log('[SpeechRecognition]', 'result', resultText);
-          this.recognitionResult.nativeElement.innerText = resultText;
+      if (this.platform.is('mobile')) {
+        this.recognition = new _window.SpeechRecognition();
+        this.initializeRecognition();
+      } else {
+        if (!('webkitSpeechRecognition' in window)) {
+          alert('¡API SpeechRecognition no soportada!');
+          return;
         }
-      };
 
-      this.recognition.onerror = (event) => {
-        console.error(event);
-      };
-
-      this.recognition.onend = () => {
-        console.log('[SpeechRecognition]', 'end');
-        this.recognizing = false;
-        this.mic = this.mic_off;
-      };
-
-    }
+        this.recognition = new _window.webkitSpeechRecognition();
+        this.initializeRecognition();
+      }
+    });
   }
 
   processSpeech() {
